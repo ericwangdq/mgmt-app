7d278fd7d8339fc9430ea139397cc2bb4d2562f5
ericwdq <wdq1103@163.com>
2018-07-28 19:31:51 +0800

    add cors testing

--- server.js
+++ server.js
@@ -3,7 +3,22 @@ const app = express();
 const path = require("path");
 const args = process.argv;
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.all(/\/api\/.*?/i, (req, res, next) => {
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-type, Accept, Content-Length, Authorization, X-Access-Token, X-Key, id-token, host');
+  res.header('Access-Control-Allow-Origin', 'http://app.eric.com'); //*
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header("Content-Type", "application/json;charset=utf-8");
+  console.log('Mock API:', req.method, req.path);
+  console.log('Cookie:', req.headers['cookie']);
+  if (req.method === 'OPTIONS') {
+      res.status(200).end();
+  } else {
+      next();
+  }
+});
+
+app.use('/', express.static(path.join(__dirname, 'build')));
 
 app.get('*', (req, res) => {
   console.log(req.method, req.path, req.headers['user-agent']);
